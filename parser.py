b0ff118e50b50cce1d8bb0e726eae42df0a0b894
Daithi <neilan.david@gmail.com>
2019-03-05 17:43:39 +0000

    Added grouping capability to Infix-Postfix converted
    * Infix-Post fix converter can now works with grouping

--- parser.py
+++ parser.py
@@ -4,7 +4,8 @@ from collections import OrderedDict
 OPERATORS = OrderedDict({
     '*': {'operands': 1, 'function': ...},
     '.': {'operands': 2, 'function': ...},
-    '|': {'operands': 2, 'function': ...}
+    '|': {'operands': 2, 'function': ...},
+
 })
 
 
@@ -15,14 +16,21 @@ def infix_to_postfix(expression: str) -> str:
     for token in expression:
 
         # Each operator is evaluated prior to being appended to the output
-        if token in OPERATORS or token in ['(', ')']:
-            while stack and list(OPERATORS).index(token) >= list(OPERATORS).index(stack[-1]):
+        if token in OPERATORS:
+            while stack and list(OPERATORS).index(token) >= (len(OPERATORS) if stack[-1] is '(' else list(OPERATORS).index(stack[-1])):
                 output.append(stack.pop())
             stack.append(token)
 
-        # All letters are appended to the output
+        elif token == '(':
+            stack.append(token)
+
+        elif token == ')':
+            while stack and stack[-1] is not '(':
+                output.append(stack.pop())
+            stack.pop()
         else:
-            output.append(token)
+            if token != '(':
+                output.append(token)
 
     # Add all remaining operators to the output
     for operator in stack[::-1]:
@@ -33,18 +41,18 @@ def infix_to_postfix(expression: str) -> str:
 
 tests = [  # abc.a|
     # Simple non grouped expressions
-    ('a.b|c.a', 'ab.ca.|'),
-    ('a.b.c*', 'ab.c*.'),
-    ('a.b|d.c*', 'ab.dc*.|'),
-    ('a.bd*', 'abd*.'),
-    ('a.b.b*.c*', 'ab.b*.c*.'),
-    ('a.b.c*', 'ab.c*.'),
+    ('a.b|c.a', 'ab.ca.|'), ('a.b.c*', 'ab.c*.'), ('a.b|d.c*', 'ab.dc*.|'), ('a.bd*', 'abd*.'),
+    ('a.b.b*.c*', 'ab.b*.c*.'), ('a.b.c*', 'ab.c*.'),
+
     # Simple expressions with grouping
-    # ('a.(b|d).c*', 'abd|.c*.'),
-    # ('(a.(b|d))*', 'abd|.*'),
-    # ('a.(b.b)*.c*', 'abb.*.c*.'),
+    ('a.(b|d).c*', 'abd|.c*.'), ('(a.(b|d))*', 'abd|.*'), ('a.(b.b)*.c*', 'abb.*.c*.'),
 ]
 
 for expression, expected_result in tests:
     result = infix_to_postfix(expression)
-    print(expression, '->', result, expected_result == result)
+    print()
+    print('Expression:', expression)
+    print('Expected Result:', expected_result)
+    print('Result:', result)
+    print('Test:', expected_result == result)
+    print()
