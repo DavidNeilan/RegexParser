de00187e5aaf05a67c647a43e4e5a7686151b346
Daithi <neilan.david@gmail.com>
2019-03-20 21:32:31 +0000

    Added NFA compilation and matching capability
    + Introduced NFA class
    + Introduced State class
    + Added ability to compile a regex expression into a saris of NFAs
    + Added ability to test a string against a regex expression

--- parser.py
+++ parser.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
 
 # Ordered by precedence where a lower index indicates a higher index
+from dataclasses import dataclass
+from typing import Optional
+
 OPERATORS = OrderedDict({
     '*': {'operands': 1, 'function': ...},
     '.': {'operands': 2, 'function': ...},
@@ -9,9 +12,19 @@ OPERATORS = OrderedDict({
 })
 
 
+class State:
+    label = None
+    edges = tuple()
+
+
+@dataclass
+class NFA:
+    initial: Optional[State] = None
+    accept: Optional[State] = None
+
+
 def infix_to_postfix(expression: str) -> str:
-    stack = []
-    output = []
+    stack, output = [], []
 
     for token in expression:
 
@@ -39,7 +52,82 @@ def infix_to_postfix(expression: str) -> str:
     return ''.join(output)
 
 
-tests = [  # abc.a|
+def compile(expression: str):
+    nfs_stack = []
+
+    for token in expression:
+
+        if token == '.':
+            nfa2, nfa1 = (nfs_stack.pop() for _ in range(2))
+
+            nfa1.accept.edges = (nfa2.initial,)
+            nfs_stack.append(NFA(nfa1.initial, nfa2.accept))
+
+        elif token == '|':
+            nfa2, nfa1 = (nfs_stack.pop() for _ in range(2))
+
+            initial = State()
+            initial.edges = (nfa1.initial, nfa2.initial)
+
+            accept = State()
+            nfa1.accept.edges = (accept,)
+            nfa2.accept.edges = (accept,)
+
+            nfs_stack.append(NFA(initial, accept))
+
+        elif token == '*':
+            nfa1 = nfs_stack.pop()
+            initial = State()
+            accept = State()
+            initial.edges = (nfa1.initial, accept)
+            nfa1.accept.edges = (nfa1.initial, accept)
+            nfs_stack.append(NFA(initial, accept))
+
+        else:
+            accept = State()
+            initial = State()
+            initial.label = token
+            initial.edges = (accept,)
+            nfs_stack.append(NFA(initial, accept))
+
+    return nfs_stack.pop()
+
+
+def match(expression: str, test_string: str) -> bool:
+    nfa = compile(infix_to_postfix(expression))
+    current_states, next_states = set(), set()
+
+    # Add the initial state to the curenent set
+    current_states = current_states.union(follow_es(nfa.initial))
+
+    for token in test_string:
+        # Loop through the set of states
+        for c in current_states:
+            if c.label == token:
+                # Add the edge1 state to the next set
+                next_states = next_states.union(follow_es(c.edges[0]))
+
+        # Set current to next, and clear out next
+        current_states = next_states.copy()
+        next_states.clear()
+
+    # Check if the accept state is in the set oc current states
+    return nfa.accept in current_states
+
+
+def follow_es(state: State):
+    """ Returns the set of states forllowing E arrows """
+
+    # Create a new set, with state as it's only member
+    states = {state}
+
+    if state.label is None:
+        for s in state.edges:
+            states = states.union(follow_es(s))
+
+    return states
+
+tests = [
     # Simple non grouped expressions
     ('a.b|c.a', 'ab.ca.|'), ('a.b.c*', 'ab.c*.'), ('a.b|d.c*', 'ab.dc*.|'), ('a.bd*', 'abd*.'),
     ('a.b.b*.c*', 'ab.b*.c*.'), ('a.b.c*', 'ab.c*.'),
@@ -48,11 +136,18 @@ tests = [  # abc.a|
     ('a.(b|d).c*', 'abd|.c*.'), ('(a.(b|d))*', 'abd|.*'), ('a.(b.b)*.c*', 'abb.*.c*.'),
 ]
 
-for expression, expected_result in tests:
-    result = infix_to_postfix(expression)
-    print()
-    print('Expression:', expression)
-    print('Expected Result:', expected_result)
-    print('Result:', result)
-    print('Test:', expected_result == result)
-    print()
+# for expression, expected_result in tests:
+#     result = infix_to_postfix(expression)
+#     print()
+#     print('Expression:', expression)
+#     print('Expected Result:', expected_result)
+#     print('Result:', result)
+#     print('Test:', expected_result == result)
+#     print()
+
+infixes = ["a.b.c*", "a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
+strings = ["", "abc", "abbc", "abcc", "abad", "abbbc"]
+
+for i in infixes:
+    for s in strings:
+        print(match(i, s), i, s)
